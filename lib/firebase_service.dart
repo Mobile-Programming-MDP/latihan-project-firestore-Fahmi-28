48e72394c3b4fb9fb8b7f321c85b06beea428bc1
vantekz <dimaszulfahmi7@gmail.com>
2024-05-02 16:34:38 +0700

    first commit

--- lib/firebase_service.dart
+++ lib/firebase_service.dart
@@ -1,36 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutterlist/userdata.dart';
 
-class FirebaseService{
+class FirebaseService {
   static final COLLECTION_REF = 'user';
 
   final firestore = FirebaseFirestore.instance;
   late final CollectionReference userRef;
 
-  FirebaseService(){
+  FirebaseService() {
     userRef = firestore.collection(COLLECTION_REF);
   }
 
 // mengambil dan menampilkan data di firebase ke ui
-  Stream<QuerySnapshot<Object?>> ambilData(){
-    return userRef.snapshots(); 
-    
+  Stream<QuerySnapshot<Object?>> ambilData() {
+    return userRef.snapshots();
   }
 
 //TAMBAH
-  void tambah(UserData userData){
-    DocumentReference documentReference = userRef.doc(userData.nama); 
+  void tambah(UserData userData) {
+    DocumentReference documentReference = userRef.doc(userData.nama);
     documentReference.set(userData.toJson());
   }
 
 //HAPUS
-  void hapus(UserData userData){
+  void hapus(UserData userData) {
     DocumentReference documentReference = userRef.doc(userData.nama);
     documentReference.delete();
   }
 
 //UBAH
-  void ubah(UserData userData){
+  void ubah(UserData userData) {
     DocumentReference documentReference = userRef.doc(userData.nama);
     documentReference.update(userData.toJson());
   }
