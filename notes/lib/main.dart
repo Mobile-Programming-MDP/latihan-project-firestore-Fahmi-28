2a68f49226140ca58f48e78853c00ed61a9b5af8
vantekz <dimaszulfahmi7@gmail.com>
2024-05-03 16:59:11 +0700

    commit

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -17,13 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: NoteListScreen()
-    );
+        title: 'Notes App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: NoteListScreen());
   }
 }
 
