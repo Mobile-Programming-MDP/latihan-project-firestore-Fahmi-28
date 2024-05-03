16475f1110b1c3745f064344025e40ba9a5de322
vantekz <dimaszulfahmi7@gmail.com>
2024-05-03 16:57:50 +0700

    main dart

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/firebase_options.dart';
+import 'package:notes/screens/note_list_screen.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NoteListScreen()
     );
   }
 }
