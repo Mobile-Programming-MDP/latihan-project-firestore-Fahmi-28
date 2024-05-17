56a7f4c4e06d5b8c21b271aea807c2771a6ad46d
Fahmi-28 <dimaszulfahmi7@gmail.com>
2024-05-17 17:04:22 +0700

    image and location

---
+++ notes/lib/services/location_service.dart
@@ -0,0 +1,25 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (serviceEnabled) {
+      return Future.error("Location Service are disable");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location Service are denied");
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          "Location Permissions are permanently denied, we cannot request permission");
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+}
